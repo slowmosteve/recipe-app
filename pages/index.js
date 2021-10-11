d877e0a41cbba97209521997f38790e7363e844c
steve choi <steve.choi@telus.com>
2021-10-10 20:23:49 -0400

    feat: home page reads recipes from Contentful

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
   const jsonAllPostsData = await getSortedPostsData()
-  console.log('getStaticProps jsonAllPostsData: '+jsonAllPostsData)
-  const allPostsData = JSON.stringify(jsonAllPostsData.sortedPosts.items)
-  console.log('getStaticProps allPostsData: '+allPostsData)
+  // console.log('getStaticProps jsonAllPostsData: '+JSON.stringify(jsonAllPostsData))
+  const allPostsData = jsonAllPostsData.sortedPosts
+  // console.log('getStaticProps allPostsData: '+allPostsData)
   return {
     props: {
       allPostsData
@@ -27,8 +27,9 @@ export async function getStaticProps() {
 //   }
 // }
 
-export default function Home({ allPostsData }) {
-  console.log('Home getSortedPostsData: '+ allPostsData)
+export default function Home({allPostsData}) {
+  // console.log('Home getSortedPostsData: '+ allPostsData)
+  // console.log('Home getSortedPostsData stringify: '+ JSON.stringify(allPostsData))
   return (
     <Layout home>
       <Head>
@@ -44,10 +45,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Recipes</h2>
         <ul className={utilStyles.list}>
-          {allPostsData}
-          
-          {/* ARRAY APPROACH */}
-          {/* {allPostsData.forEach(element => {
+          {allPostsData.map(({ slug, date, title }) => (
             <li className={utilStyles.listItem} key={slug}>
             <Link href={`/posts/${slug}`}>
               <a>{title}</a>
@@ -57,19 +55,7 @@ export default function Home({ allPostsData }) {
               <Date dateString={date} />
             </small>
           </li>
-          })} */}
-          {/* ORIGINAL APPROACH */}
-          {/* {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))} */}
+          ))}
         </ul>
       </section>
     </Layout>
